999e407f99d28f8fa6267f55a88d1e75fb5143ee
Eduard Urbach <e.urbach@gmail.com>
2017-06-17 22:35:30 +0200

    Support new session API

--- MemoryStore.go
+++ MemoryStore.go
@@ -1,6 +1,8 @@
 package memstore
 
 import (
+	"errors"
+
 	"github.com/aerogo/session"
 	"golang.org/x/sync/syncmap"
 )
@@ -17,17 +19,18 @@ func New() *MemoryStore {
 }
 
 // Get a session by its ID.
-func (store *MemoryStore) Get(id string) *session.Session {
+func (store *MemoryStore) Get(id string) (*session.Session, error) {
 	value, ok := store.sessions.Load(id)
 
 	if !ok {
-		return nil
+		return nil, errors.New("Session not found")
 	}
 
-	return value.(*session.Session)
+	return value.(*session.Session), nil
 }
 
 // Set saves a session so it can be retrieved by its ID.
-func (store *MemoryStore) Set(id string, session *session.Session) {
+func (store *MemoryStore) Set(id string, session *session.Session) error {
 	store.sessions.Store(id, session)
+	return nil
 }
